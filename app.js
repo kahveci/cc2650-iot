8e652411e3326b377fa73679c3b8d48ce53f31f7
Sinan Kahveci <sinan.kahveci@windowslive.com>
2019-10-04 18:26:39 +0100

    [FEATURE] Add OPC UA server support

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const sensorTag = require('sensortag');
+const opcuaServer = require('@wmg/opcua-server');
 const config = require('./config.json');
 const analytics = require('./analytics');
 const influxdb = require('./influxdb');
 
-console.log('Listening for sensortags...');
+console.log('Sensor Tag: Listening for sensortags...');
+
+(function initialiseOpcUaServer() {
+  if (config.opcuaServer.enabled === true) {
+    opcuaServer.run(config);
+  }
+}());
+
+function updateOpcUAServerVariable(opcTag, opcValue) {
+  if (config.opcuaServer.enabled === true) {
+    opcuaServer.updateValue(opcTag, opcValue);
+  }
+}
 
 sensorTag.discoverById(config.sensortag.id, (tag) => {
   console.log(`Sensortag discovered: ${tag}`);
@@ -29,26 +42,33 @@ sensorTag.discoverById(config.sensortag.id, (tag) => {
     });
   }
 
-  // When you get an accelermeter change, print it out:
   function listenIrTemperature() {
     tag.on('irTemperatureChange', (objectTemp, ambientTemp) => {
-      console.log('* * TMP007 IR (infrared) thermopile temperature sensor * *');
-      console.log('\tObject Temp  = %d °C', objectTemp.toFixed(1));
-      console.log('\tAmbient Temp = %d °C', ambientTemp.toFixed(1));
-      analytics.insertTemperature(objectTemp.toFixed(1), ambientTemp.toFixed(1));
-      influxdb.insertTemperature(objectTemp.toFixed(1), ambientTemp.toFixed(1));
+      if (config.sensortag.consoleOutput === 'enabled') {
+        console.log('* * TMP007 IR (infrared) thermopile temperature sensor * *');
+        console.log('\tObject Temp  = %d °C', objectTemp.toFixed(2));
+        console.log('\tAmbient Temp = %d °C', ambientTemp.toFixed(2));
+      }
+      updateOpcUAServerVariable('temperature', ambientTemp.toFixed(2));
+      analytics.insertTemperature(objectTemp.toFixed(2), ambientTemp.toFixed(2));
+      influxdb.insertTemperature(objectTemp.toFixed(2), ambientTemp.toFixed(2));
     });
   }
 
-  // When you get an accelermeter change, print it out:
+  // When you get an accelerometer change, print it out:
   function listenAccelerometer() {
     tag.on('accelerometerChange', (x, y, z) => {
       if (x !== 0 && y !== 0 && z !== 0) {
-        console.log('* * MPU-9450 9-axis motion sensor (3-AXIS Accelerometer) * *');
-        console.log('\tx = %d G', x.toFixed(2));
-        console.log('\ty = %d G', y.toFixed(2));
-        console.log('\tz = %d G', z.toFixed(2));
+        if (config.sensortag.consoleOutput === 'enabled') {
+          console.log('* * MPU-9450 9-axis motion sensor (3-AXIS Accelerometer) * *');
+          console.log('\tx = %d G', x.toFixed(2));
+          console.log('\ty = %d G', y.toFixed(2));
+          console.log('\tz = %d G', z.toFixed(2));
+        }
         analytics.insertAccelerometer(x.toFixed(2), y.toFixed(2), z.toFixed(2));
+        updateOpcUAServerVariable('accelerometerX', x.toFixed(2));
+        updateOpcUAServerVariable('accelerometerY', y.toFixed(2));
+        updateOpcUAServerVariable('accelerometerZ', z.toFixed(2));
       }
     });
   }
@@ -56,10 +76,12 @@ sensorTag.discoverById(config.sensortag.id, (tag) => {
   function listenGyroscope() {
     tag.on('gyroscopeChange', (x, y, z) => {
       if (x !== 0 && y !== 0 && z !== 0) {
-        console.log('* * MPU-9450 9-axis motion sensor (3-AXIS Gyroscope) * *');
-        console.log('\tx = %d °/s', x.toFixed(2));
-        console.log('\ty = %d °/s', y.toFixed(2));
-        console.log('\tz = %d °/s', z.toFixed(2));
+        if (config.sensortag.consoleOutput === 'enabled') {
+          console.log('* * MPU-9450 9-axis motion sensor (3-AXIS Gyroscope) * *');
+          console.log('\tx = %d °/s', x.toFixed(2));
+          console.log('\ty = %d °/s', y.toFixed(2));
+          console.log('\tz = %d °/s', z.toFixed(2));
+        }
       }
     });
   }
@@ -67,36 +89,46 @@ sensorTag.discoverById(config.sensortag.id, (tag) => {
   function listenMagnetometer() {
     tag.on('magnetometerChange', (x, y, z) => {
       if (x !== 0 && y !== 0 && z !== 0) {
-        console.log('* * MK24 magnetic sensor * *');
-        console.log('\tx = %d μT', x.toFixed(1));
-        console.log('\ty = %d μT', y.toFixed(1));
-        console.log('\tz = %d μT', z.toFixed(1));
+        if (config.sensortag.consoleOutput === 'enabled') {
+          console.log('* * MK24 magnetic sensor * *');
+          console.log('\tx = %d μT', x.toFixed(1));
+          console.log('\ty = %d μT', y.toFixed(1));
+          console.log('\tz = %d μT', z.toFixed(1));
+        }
       }
     });
   }
 
   function listenHumidity() {
     tag.on('humidityChange', (temperature, humidity) => {
-      console.log('* * HDC1000 digital humidity and temperature sensor * *');
-      console.log('\tTemperature = %d °C', temperature.toFixed(1));
-      console.log('\tHumidity    = %d %', humidity.toFixed(1));
+      if (config.sensortag.consoleOutput === 'enabled') {
+        console.log('* * HDC1000 digital humidity and temperature sensor * *');
+        console.log('\tTemperature = %d °C', temperature.toFixed(2));
+        console.log('\tHumidity    = %d %', humidity.toFixed(2));
+      }
+      updateOpcUAServerVariable('luxometer', lux.toFixed(2));
     });
   }
 
   function listenBarometricPressure() {
     tag.on('barometricPressureChange', (pressure) => {
-      // TODO: Check the temperature value of this sensor!
-      console.log('* * BMP280 Barometric pressure & temperature sensor * *');
-      console.log('\tPressure = %d mBar', pressure.toFixed(1));
+      if (config.sensortag.consoleOutput === 'enabled') {
+        // TODO: Check the temperature value of this sensor!
+        console.log('* * BMP280 Barometric pressure & temperature sensor * *');
+        console.log('\tPressure = %d mBar', pressure.toFixed(1));
+      }
     });
   }
 
   function listenLuxometer() {
     tag.on('luxometerChange', (lux) => {
       if (lux !== 134184.96) {
-        console.log('* * OPT3001 Ambient light sensor * *');
-        console.log('\tLux = %d', lux.toFixed(1));
-        analytics.insertLuxometer(lux.toFixed(1));
+        if (config.sensortag.consoleOutput === 'enabled') {
+          console.log('* * OPT3001 Ambient light sensor * *');
+          console.log('\tLux = %d', lux.toFixed(2));
+        }
+        updateOpcUAServerVariable('luxometer', lux.toFixed(2));
+        analytics.insertLuxometer(lux.toFixed(2));
       }
     });
   }
@@ -135,18 +167,19 @@ sensorTag.discoverById(config.sensortag.id, (tag) => {
 
   function enableSensors() { // attempt to enable sensors
     tag.notifySimpleKey(listenForButton); // start the button listener
-    console.log('Enabling sensors...');
+    console.log('Sensor Tag: enabling sensors...');
 
     // when you enable the sensors, start notifications:
-    if (config.sensortag.sensor.IrTemperature === 'enabled') tag.enableIrTemperature(notifyIrTemperature);
-    if (config.sensortag.sensor.Accelerometer === 'enabled') tag.enableAccelerometer(notifyAccelerometer);
-    if (config.sensortag.sensor.Gyroscope === 'enabled') tag.enableGyroscope(notifyGyroscope);
-    if (config.sensortag.sensor.Magnetometer === 'enabled') tag.enableMagnetometer(notifyMagnetometer);
-    if (config.sensortag.sensor.Humidity === 'enabled') tag.enableHumidity(notifyHumidity);
-    if (config.sensortag.sensor.BarometricPressure === 'enabled') tag.enableBarometricPressure(notifyBarometricPressure);
-    if (config.sensortag.sensor.Luxometer === 'enabled') tag.enableLuxometer(notifyLuxometer);
+    if (config.sensortag.sensor.irTemperature === 'enabled') tag.enableIrTemperature(notifyIrTemperature);
+    if (config.sensortag.sensor.accelerometer === 'enabled') tag.enableAccelerometer(notifyAccelerometer);
+    if (config.sensortag.sensor.gyroscope === 'enabled') tag.enableGyroscope(notifyGyroscope);
+    if (config.sensortag.sensor.magnetometer === 'enabled') tag.enableMagnetometer(notifyMagnetometer);
+    if (config.sensortag.sensor.humidity === 'enabled') tag.enableHumidity(notifyHumidity);
+    if (config.sensortag.sensor.barometricPressure === 'enabled') tag.enableBarometricPressure(notifyBarometricPressure);
+    if (config.sensortag.sensor.luxometer === 'enabled') tag.enableLuxometer(notifyLuxometer);
     // SP40641LU Digital microphone
     // Buzzer
+    console.log('Sensor Tag: selected sensors enabled successfully!');
   }
 
   (function connectAndSetUp() { // attempt to connect to the tag
